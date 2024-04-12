ce967a2c84ecb4621b0ef1747a36bb2604567883
Uzair <uzairnaeem@folio3.com>
2024-04-12 09:45:17 -0500

    DataVisualizationProject

---
+++ barchart.js
@@ -0,0 +1,46 @@
+// Set the dimensions and margins of the graph
+const margin = { top: 30, right: 30, bottom: 70, left: 60 },
+    width = 800 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+// Append the svg object to the body of the page
+const svg = d3.select("#d3barchart")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+// Parse the Data
+d3.csv("data.csv").then(function (data) {
+
+    // X axis
+    const x = d3.scaleBand()
+        .range([0, width])
+        .domain(data.map(d => d.country))
+        .padding(0.2);
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+        .attr("transform", "translate(-10,0)rotate(-45)")
+        .style("text-anchor", "end");
+
+    // Add Y axis
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(data, d => +d.population)])
+        .range([height, 0]);
+    svg.append("g")
+        .call(d3.axisLeft(y));
+
+    // Bars
+    svg.selectAll("mybar")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("x", d => x(d.country))
+        .attr("width", x.bandwidth())
+        .attr("y", d => y(d.population))
+        .attr("height", d => height - y(d.population));
+});
